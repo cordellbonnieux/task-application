5bcd8a865fbf8872f7c59ac5a6b091bdae089f2d
Cordell <cordellbonnieux@gmail.com>
2022-06-10 16:16:06 -0700

    Started task app project.

---
+++ src/components/Overview.js
@@ -0,0 +1,12 @@
+export default class Overview {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
