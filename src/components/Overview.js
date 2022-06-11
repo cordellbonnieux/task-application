c056deeb5734fde7f9638f92faa2b1e7163a03e5
Cordell <cordellbonnieux@gmail.com>
2022-06-10 17:32:12 -0700

    Completed challenge 2, items can now be deleted.

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,8 +1,28 @@
 import React from "react"
 
-export default class Overview extends React.Component {    
+export default class Overview extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { banned: [] }
+    }
+
+    delete = (key) => {
+        this.setState({
+            banned: [...this.state.banned, key]
+        })
+    }
+
     render() {
-        const list = this.props.tasks.map(task => <li key={task.key}>{task.name + '  (' + task.count + ')'}</li>)
+        const list = this.props.tasks.map(task => {
+            if (!this.state.banned.includes(task.key)) {
+                return <li key={task.key}>
+                    {task.name + '  (' + task.count + ')'}
+                    <button type="button" onClick={() => this.delete(task.key)}>delete item</button>
+                </li>
+            } else {
+                return null
+            }
+        })
         return (
             <div>
                 <ul>
