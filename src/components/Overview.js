23d3ecbaafe5f183de91272a6ef0b20229f7a4f6
Cordell <cordellbonnieux@gmail.com>
2022-06-12 00:59:27 -0700

    Clicking edit now changes into editable field.

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -13,13 +13,32 @@ export default class Overview extends React.Component {
     }
 
     edit = (key) => {
+        for (let i = 0; i < this.state.tasks.length; i++) {
+            if (this.state.tasks[i].key === key) {
+                let arr = this.state.tasks
+                arr[i].editing = true
+                this.setState({
+                    tasks: arr
+                })
+            }
+        }
+    }
+
+    save = (key) => {
 
     }
 
     render() {
         const list = this.state.tasks.map(task => {
-            const name = task.editing ? <input type="text" id={task.key}></input> : <span>{task.name + '  (' + task.count + ')'}</span>
-            const edit = task.editing ? <button type="text">save</button> : <button type="button" onClick={() => this.edit(task.key)}>edit</button>
+
+            const name = task.editing ? 
+                <input type="text" id={task.key}></input> : 
+                <span>{task.name + '  (' + task.count + ')'}</span>
+
+            const edit = task.editing ? 
+                <button type="text" onClick={() => this.save(task.key)}>save</button> : 
+                <button type="button" onClick={() => this.edit(task.key)}>edit</button>
+
             if (!this.state.banned.includes(task.key)) {
                 return <li key={task.key}>
                     {name}
